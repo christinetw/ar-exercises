0b8f8f6d5f5ec4ce5c643bf7291b2238eb7ae0e3
Christine Lo <christine.lo.hayes@gmail.com>
2021-11-08 22:20:55 -0500

    ar-exercises

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,9 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employee.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employee.create(first_name: "Wyatt", last_name: "Pokemons", hourly_rate: 100)
+@store2.employee.create(first_name: "Hans", last_name: "Mcgram", hourly_rate: 45)
+@store2.employee.create(first_name: "Paige", last_name: "Kim", hourly_rate: 53)
+@store1.employee.create(first_name: "Ian", last_name: "Apple", hourly_rate: 500)
+@store1.employee.create(first_name: "Brendan", last_name: "luo", hourly_rate: 40)
